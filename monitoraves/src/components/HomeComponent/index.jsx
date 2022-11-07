98c1bf3077353e009309ec7e62495e5076c40adb
Jhon Muller <jmuller.jhon80@gmail.com>
2022-11-06 21:13:17 -0400

    feat: animacao adicionada a homecomponent

--- monitoraves/src/components/HomeComponent/index.jsx
+++ monitoraves/src/components/HomeComponent/index.jsx
@@ -10,16 +10,15 @@ const HomeComponent = () => {
 
     return(
         <div className="container-homeComponent">
-            <p className="texto-home">Monitore sua produção com dados importantes para tomada de decisões.</p>
-            <p className="texto-home">O melhor do aviário com o auxílio da tecnologia.</p>
-            <div className="box-ovo-frango">
+            <p className="texto-home" data-aos="fade-right" >Monitore sua produção com dados importantes para tomada de decisões.</p>
+            <p className="texto-home" data-aos="fade-right">O melhor do aviário com o auxílio da tecnologia.</p>
+            <div className="box-ovo-frango" data-aos="fade-left">
                 <img className="ovos" src={Ovos} alt="imagem de ovos em uma cartela" />
                 <img className="frango" src={Frango} alt="imagem de um frango olhando para a câmera" />
             </div>
-            <button className="minhaGranja" onClick={()=> navigate('/resumo')} >
+            <button className="minhaGranja" onClick={()=> navigate('/resumo')} data-aos="fade-up" >
                 Minha Granja
             </button>
-
         </div>
     )
 }
