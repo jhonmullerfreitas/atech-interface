91266685a92a9e5f07c4b7ea88bff69206c58cbb
Jhon Muller <jmuller.jhon80@gmail.com>
2022-11-06 20:53:22 -0400

    feat: home estruturada

--- monitoraves/src/components/BasePageComponent/index.jsx
+++ monitoraves/src/components/BasePageComponent/index.jsx
@@ -1,14 +1,19 @@
 import "./styles.css"
 import LogoMonitoraves from "../../assets/LogoLogoMonitoraves.png"
 import MinhaConta from "../../assets/MinhaConta.png"
+import { useNavigate } from "react-router-dom"
 
 const BasePage = ({children}) =>{
+
+    const navigate = useNavigate()
+
+
     return(
         <div className="container-basepage">
             <header className="cabecalho">
-                <img className="logoMonitorave" src={LogoMonitoraves} alt="" />
+                <img className="logoMonitorave" src={LogoMonitoraves} alt="Logo da Monitoraves representado por um frango em cima do nome monitoraves" onClick={()=> navigate("/")} />
                 <ul className="menu-cabecalho">
-                    <li>Home</li>
+                    <li onClick={()=> navigate("/")} >Home</li>
                     <li>Sobre</li>
                     <li>Contato</li>
                 </ul>
@@ -18,19 +23,19 @@ const BasePage = ({children}) =>{
 
             <nav className="menu-lateral">
                 <ul className="opcao-menu-lateral">
-                    <h5 className="resumo">Resumo</h5>
+                    <h5 className="resumo" onClick={()=> navigate("/resumo")} >Resumo</h5>
                     <li>Informações da granja</li>
                 </ul>
                 <ul className="opcao-menu-lateral">
-                    <h5 className="lotes">Lotes</h5>
+                    <h5 className="lotes" onClick={()=> navigate("/lotes")} >Lotes</h5>
                     <li>Informações da granja</li>
                 </ul>
                 <ul className="opcao-menu-lateral">
-                    <h5 className="graficos">Gráficos</h5>
+                    <h5 className="graficos" onClick={()=> navigate("/graficos")} >Gráficos</h5>
                     <li>Informações da granja</li>
                 </ul>
                 <ul className="opcao-menu-lateral">
-                    <h5 className="tabelas">Tabelas</h5>
+                    <h5 className="tabelas" onClick={()=> navigate("/tabelas")} >Tabelas</h5>
                     <li>Informações da granja</li>
                 </ul>
             </nav>
